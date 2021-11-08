d8f59dd26f92d4485d007f1efb9827d2b65fa54a
Cam Dowless <cameron.dowless@trustasc.com>
2021-11-08 17:04:51 -0500

    Update Filenames etc

---
+++ API/Controllers/FileController.cs
@@ -0,0 +1,68 @@
+﻿using API_Test;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+namespace BulkAPITest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FileController : ControllerBase
+    {
+        private readonly ILogger<AccountController> _logger;
+        FileHelper FH = new FileHelper();
+        public FileController(ILogger<AccountController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost, DisableRequestSizeLimit]
+        [Route("Upload")]
+        public async Task<IActionResult> Upload()
+        {
+            List<SavedFile> created = new List<SavedFile>();
+            try
+            {
+                var formCollection = await Request.ReadFormAsync();
+                foreach (IFormFile file in formCollection.Files)
+                {
+                    var folderName = "Files";
+                    var directoryToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
+                    if (file.Length > 0)
+                    {            
+                        var name = ContentDispositionHeaderValue.Parse(file.ContentDisposition).Name.Trim('"');
+                        var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+                      
+                        var fullPath = Path.Combine(directoryToSave, fileName);
+                        var dbPath = Path.Combine(folderName, fileName);
+
+                        byte[] bytes = FH.ReadFully(file.OpenReadStream());
+                        Console.WriteLine(bytes.Length);
+                        //File.WriteAllBytes(bytes, fullPath);
+
+                        /*using (var stream = new FileStream(fullPath, FileMode.Create))
+                        {
+                            file.CopyTo(stream);
+                            
+                        }*/
+                        //return Ok(new { dbPath });
+                    }
+                    else
+                    {
+                        return BadRequest();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, $"Internal Server Error: {e}");
+            }
+            return Ok();
+        }
+
+    }
+}
---
+++ API/Controllers/QRCodeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Test.Controllers
+{   
+    [ApiController]
+    [Route("[controller]")]
+    public class QRCodeController : Controller
+    {
+        private readonly ILogger<QRCodeController> _logger;
+
+        public QRCodeController(ILogger<QRCodeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get(string URL)
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(URL, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(10);
+            byte[] arr = ToByteArray(qrCodeImage, ImageFormat.Jpeg);
+            return File(arr, "image/jpeg");
+        }
+
+        public static byte[] ToByteArray( Image image, ImageFormat format)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                image.Save(ms, format);
+                return ms.ToArray();
+            }
+        }
+    }
+}
