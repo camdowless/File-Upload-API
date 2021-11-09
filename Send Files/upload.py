35db0fceff0d9116dbe86aa9b5be3f0be288b7e4
Cam Dowless <cameron.dowless@trustasc.com>
2021-11-09 10:52:53 -0500

    Update VS Project

--- Send Files/upload.py
+++ Send Files/upload.py
@@ -7,7 +7,7 @@ files = {}
 for f in next(walk("upload"), (None, None, []))[2]:
     with open(f'upload/{f}', 'rb') as current:
         files[f] = encoded_string = base64.b64encode(current.read())
-
+print(len(files))
 proxies = {'http': 'https://localhost:5001'}
 response = r.post(url, files=files, verify=False, proxies=proxies)
 print(response.status_code)
